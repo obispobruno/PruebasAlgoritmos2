ae00140387f34edf9425491f44da4b0f31569480
Bruno <brunoobispo@duck.com>
2023-09-09 16:44:27 -0300

    Better function

--- pruebas.sh
+++ pruebas.sh
@@ -1,43 +1,36 @@
 #!/bin/bash
 
 correrPrueba() {
-  echo '---------------------------'
-  echo "Prueba $2.in.txt:"
-  ./ejercicio$1.out < $2.in.txt > $2.out.mine.txt
-  output=$(diff --strip-trailing-cr $2.out.mine.txt $2.out.txt)
-  if [ -z "$output" ]; then
-    echo "OK"
-  else
-    echo "ERROR"
-    diff --strip-trailing-cr $2.out.mine.txt $2.out.txt
+  if [ -z $3 ]; then
+    echo '---------------------------'
+    echo "    Pruebas Ejercicio $1    "
   fi
+  g++ -std=c++11 ejercicio${1}.cpp -o ejercicio${1}.out
+  if  [ $? -ne 0 ]; then
+    echo "--- Error de compilación. Abortando prueba ---"
+    exit 1
+  fi
+  for ((i = 10; i <= 1000000; i*=10)); do
+    echo '---------------------------'
+    echo "Prueba $2${i}.in.txt:"
+    ./ejercicio$1.out < $2${i}.in.txt > $2${i}.out.mine.txt
+    output=$(diff --strip-trailing-cr $2${i}.out.mine.txt $2${i}.out.txt)
+    if [ -z "$output" ]; then
+      echo "OK"
+    else
+      echo "ERROR"
+      diff --strip-trailing-cr $2${i}.out.mine.txt $2${i}.out.txt
+    fi
+  done
 }
 
 if [ -f ejercicio1.cpp ]; then
-  echo '---------------------------'
-  echo '    Pruebas Ejercicio 1    '
-  g++ -std=c++11 ejercicio1.cpp -o ejercicio1.out
-  if  [ $? -ne 0 ]; then
-    echo "--- Error de compilación. Abortando prueba ---"
-    exit 1
-  fi
-  for ((i = 10; i <= 1000000; i*=10)); do
-    correrPrueba 1 ${i}
-  done
+  correrPrueba 1
 fi
 
 if [ -f ejercicio2.cpp ]; then
-  echo '---------------------------'
-  echo '    Pruebas Ejercicio 2    '
-  g++ -std=c++11 -o ejercicio2.out ejercicio2.cpp
-  if  [ $? -ne 0 ]; then
-    echo "--- Error de compilación. Abortando prueba ---"
-    exit 1
-  fi
-  for ((i = 10; i <= 1000000; i*=10)); do
-    correrPrueba 2 2_${i}
-    correrPrueba 2 5_${i}
-  done
+  correrPrueba 2 2_
+  correrPrueba 2 5_ 0
 fi
 
 rm *.out.mine.txt
