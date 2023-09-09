afc7b93499deadcc6d7233879d00771a9770df8a
Bruno <brunoobispo@duck.com>
2023-09-09 13:55:57 -0300

    Added function to run each test

--- pruebas.sh
+++ pruebas.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 
+correrPrueba() {
+  echo '---------------------------'
+  echo "Prueba $2.in.txt:"
+  ./ejercicio$1.out < $2.in.txt > $2.out.mine.txt
+  output=$(diff --strip-trailing-cr $2.out.mine.txt $2.out.txt)
+  if [ -z "$output" ]; then
+    echo "OK"
+  else
+    echo "ERROR"
+    diff --strip-trailing-cr $2.out.mine.txt $2.out.txt
+  fi
+}
+
 if [ -f ejercicio1.cpp ]; then
   echo '---------------------------'
   echo '    Pruebas Ejercicio 1    '
   g++ -std=c++11 ejercicio1.cpp -o ejercicio1.out
+  if  [ $? -ne 0 ]; then
+    echo "Error de compilación"
+    exit 1
+  fi
   for ((i = 10; i <= 1000000; i*=10)); do
-    ./ejercicio1.out < ${i}.in.txt > ${i}.out.mine.txt
-    output=$(diff --strip-trailing-cr ${i}.out.mine.txt ${i}.out.txt)
-    echo '---------------------------'
-    echo "Prueba ${i}.in.txt:"
-    if [ -z "$output" ]; then
-      echo "OK"
-    else
-      echo "ERROR"
-      diff --strip-trailing-cr -y ${i}.out.mine.txt ${i}.out.txt
-    fi
+    correrPrueba 1 ${i}
   done
 fi
 
@@ -22,27 +30,13 @@ if [ -f ejercicio2.cpp ]; then
   echo '---------------------------'
   echo '    Pruebas Ejercicio 2    '
   g++ -std=c++11 -o ejercicio2.out ejercicio2.cpp
+  if  [ $? -ne 0 ]; then
+    echo "Error de compilación"
+    exit 1
+  fi
   for ((i = 10; i <= 1000000; i*=10)); do
-    ./ejercicio2.out < 2_${i}.in.txt > 2_${i}.out.mine.txt
-    ./ejercicio2.out < 5_${i}.in.txt > 5_${i}.out.mine.txt
-    output=$(diff --strip-trailing-cr 2_${i}.out.mine.txt 2_${i}.out.txt)
-    echo '---------------------------'
-    echo "Prueba 2_${i}.in.txt:"
-    if [ -z "$output" ]; then
-      echo "OK"
-    else
-      echo "ERROR"
-      diff --strip-trailing-cr -y 5_${i}.out.mine.txt 5_${i}.out.txt
-    fi
-    output=$(diff --strip-trailing-cr 5_${i}.out.mine.txt 5_${i}.out.txt)
-    echo '---------------------------'
-    echo "Prueba 5_${i}.in.txt:"
-    if [ -z "$output" ]; then
-      echo "OK"
-    else
-      echo "ERROR"
-      diff --strip-trailing-cr -y 5_${i}.out.mine.txt 5_${i}.out.txt
-    fi
+    correrPrueba 2 2_${i}
+    correrPrueba 2 5_${i}
   done
 fi
 
