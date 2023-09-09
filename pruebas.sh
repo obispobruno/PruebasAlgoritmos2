93b5c8f303c8d2b9ce8d0c3409e23b6864db7a57
Bruno <brunoobispo@duck.com>
2023-09-08 22:55:12 -0300

    Better loop

--- pruebas.sh
+++ pruebas.sh
@@ -4,7 +4,7 @@ if [ -f ejercicio1.cpp ]; then
   echo '---------------------------'
   echo '    Pruebas Ejercicio 1    '
   g++ -std=c++11 ejercicio1.cpp -o ejercicio1.out
-  for ((i = 10; i <= 1000000; i*=10)) do
+  for ((i = 10; i <= 1000000; i*=10)); do
     ./ejercicio1.out < ${i}.in.txt > ${i}.out.mine.txt
     output=$(diff --strip-trailing-cr ${i}.out.mine.txt ${i}.out.txt)
     echo '---------------------------'
@@ -22,7 +22,7 @@ if [ -f ejercicio2.cpp ]; then
   echo '---------------------------'
   echo '    Pruebas Ejercicio 2    '
   g++ -std=c++11 -o ejercicio2.out ejercicio2.cpp
-  for ((i = 10; i <= 1000000; i*=10)) do
+  for ((i = 10; i <= 1000000; i*=10)); do
     ./ejercicio2.out < 2_${i}.in.txt > 2_${i}.out.mine.txt
     ./ejercicio2.out < 5_${i}.in.txt > 5_${i}.out.mine.txt
     output=$(diff --strip-trailing-cr 2_${i}.out.mine.txt 2_${i}.out.txt)
